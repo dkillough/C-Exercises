2784ba05bd2431fb2d3bc2df4d7fb92582615bb5
dkillough <dkillough@utexas.edu>
2020-01-06 00:32:20 -0500

    modification and cleanup

--- exercises.c
+++ exercises.c
@@ -2,9 +2,6 @@
    exercises.c program
    simple exercises to help learn c made by @dkillough.
    sections based off of section tutorials from learn-c.org (copyright n.d.)
-
-   //issue 1 @2.printf: slightly unintended behavior, likely due to casting ints
-   to float
  */
 
 #include <stdio.h>
@@ -13,20 +10,27 @@ int main() {
 
   // // 1. Hello World
   // /* write a simple hello world statement in c */
+  //
   // printf("Hello, world!");
 
   // 2. Vars/types
-  // /* */
+  // /* write a statement to add the following values together, then print them
+  // out */
   // int varI = 6;
   // float varF = -561.99;
+  // double varD = 12.1;
   // int var4 = 429;
-  // float theSum = varI + varF + var4;
-  // printf("Let's add 4 nums: %f", theSum);
+  //
+  // float theSum = varI + varD + varF + var4;
+  // printf("Let's add 4 nums: %f\n", theSum);
 
   // // 3. Arrays
-  // /* init an array with 10 elements, then fill them with alternating values
-  // 90 and 100. Then print the average. */ int grades[10]; int index,
-  // runningTotal = 0; for (index = 0; index < 10; index++) {
+  // /* init an array with 10 elements, then fill it with alternating values
+  // 90 and 100. Then print the average. */
+  //
+  // int grades[10];
+  // int index, runningTotal = 0;
+  // for (index = 0; index < 10; index++) {
   //   if (index % 2 == 0) {
   //     grades[index] = 100;
   //   } else {
