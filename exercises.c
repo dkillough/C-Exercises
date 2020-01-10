1ec477ee982b1c5e71c7cad76e39c296f2473a80
dkillough <dkillough@utexas.edu>
2020-01-09 22:19:11 -0500

    Added section 7: For loop Reconfigured layout

--- exercises.c
+++ exercises.c
@@ -4,6 +4,7 @@
    sections based off of section tutorials from learn-c.org (copyright n.d.)
  */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,98 +13,136 @@
 #define FALSE 0
 #define TRUE 1
 
+int one(), two(), three(), four(), five(), six(), seven();
+
 int main() {
+  one();
+  two();
+  three();
+  four();
+  five();
+  six();
+  seven();
+  return 0;
+}
 
-  // // 1. Hello World
-  // /* write a simple hello world statement in c */
-  //
-  // printf("Section 1\nHello, world!");
+// 1. Hello World
+/* write a simple hello world statement in c using printf */
+int one() {
+  printf("Section 1\nHello, world!\n\n");
+  return 0;
+}
 
-  // // 2. Vars/types
-  // /* write a statement to add the following values together, then print them
-  // out */
-  // int varI = 6;
-  // float varF = -561.99;
-  // double varD = 12.1;
-  // int var4 = 429;
-  //
-  // float theSum = varI + varD + varF + var4;
-  // printf("Section 2\nLet's add 4 nums: %f\n", theSum);
+// 2. Vars/types
+/* write a statement to add the following values together, then print them out
+ */
+int two() {
+  int varI = 6;
+  float varF = -561.99;
+  double varD = 12.1;
+  int var4 = 429;
 
-  // // 3. Arrays
-  // /* init an array with 10 elements, then fill it with alternating values
-  // 90 and 100. Then print the average.
-  // Don't forget to additionally initialize index and runningTotal variables.
-  // */
-  //
-  // int array[10];
-  // int index, runningTotal = 0;
-  // for (index = 0; index < 10; index++) {
-  //   if (index % 2 == 0) {
-  //     array[index] = 100;
-  //   } else {
-  //     array[index] = 90;
-  //   }
-  // }
-  // for (index = 0; index < 10; index++) {
-  //   runningTotal += array[index];
-  // }
-  // printf("Section 3\nArray average: %i\n", runningTotal / 10);
+  float theSum = varI + varD + varF + var4;
+  printf("Section 2\nLet's add 4 nums: %f\n\n", theSum);
+  return 0;
+}
 
-  // // 4. Multidimensional Arrays
-  // // /* initialize 2 2D arrays. Loop through each and compare values at each
-  // // index. Print the number of times the values at each index are the same
-  // */
-  //
-  // int array2D[2][4] = {{0, 1, 2, 3}, {4, 5, 6, 7}};
-  // int array2D2[2][4] = {{0, 4, 7, 1}, {1, 4, 6, 9}};
-  // int i, j, runningTotal = 0;
-  // for (i = 0; i < 2; i++) {
-  //   for (j = 0; j < 4; j++) {
-  //     runningTotal += (array2D[i][j] == array2D2[i][j]);
-  //   }
-  // }
-  // printf("Section 4\nCount equalities: %i\n", runningTotal);
+// 3. Arrays
+/* init an array with 10 elements, then fill it with alternating values
+90 and 100. Then print the average.
+Don't forget to additionally initialize index and runningTotal variables. */
+int three() {
+  int array[10];
+  int index, runningTotal = 0;
+  for (index = 0; index < 10; index++) {
+    if (index % 2 == 0) {
+      array[index] = 100;
+    } else {
+      array[index] = 90;
+    }
+  }
+  for (index = 0; index < 10; index++) {
+    runningTotal += array[index];
+  }
+  printf("Section 3\nArray average: %i\n\n", runningTotal / 10);
+  return 0;
+}
 
-  // // 5. Conditions
-  // /* complete a number guessing game with the logic to tell the player
-  // whether to go higher or lower. */
-  // int guessed = FALSE;
-  // int numToGuess = rand() % 100;
-  // int numUserGuessed;
-  //
-  // while (guessed == FALSE) {
-  //   printf("Enter a number: ");
-  //   scanf("%d", &numUserGuessed);
-  //   printf("You guessed: %i\n", numUserGuessed);
-  //
-  //   if (numUserGuessed == numToGuess) {
-  //     guessed = TRUE;
-  //   } else if (numUserGuessed < numToGuess) {
-  //     printf("Guess higher.\n");
-  //   } else {
-  //     printf("Guess lower.\n");
-  //   }
-  //
-  // }
-  // printf("Section 5\nThe number was guessed: %i\n", numToGuess);
+// 4. Multidimensional Arrays
+/* initialize 2 2D arrays. Loop through each and compare values at each
+index. Print the number of times the values at each index are the same */
+int four() {
+  int array2D[2][4] = {{0, 1, 2, 3}, {4, 5, 6, 7}};
+  int array2D2[2][4] = {{0, 4, 7, 1}, {1, 4, 6, 9}};
+  int i, j, runningTotal = 0;
+  for (i = 0; i < 2; i++) {
+    for (j = 0; j < 4; j++) {
+      runningTotal += (array2D[i][j] == array2D2[i][j]);
+    }
+  }
+  printf("Section 4\nCount equalities: %i\n\n", runningTotal);
+  return 0;
+}
 
-  // // 6. Strings
-  // /* make a password guesser based on set password and an input string of a maximum of 20 characters */
-  // char *setPassword = "password";
-  // char input[20];
-  // int guessed = FALSE;
-  //
-  // while (guessed == FALSE) {
-  //   printf("What's the password? Max 19 characters: ");
-  //   scanf("%s", input);
-  //   if (strncmp(setPassword, input, 20) == 0) {
-  //     guessed = TRUE;
-  //   } else {
-  //     printf("Incorrect password. Please try again\n");
-  //   }
-  // }
-  // printf("Section 6\nPassword Accepted\n");
+// 5. Conditions
+/* complete a number guessing game with the logic to tell the player
+whether to go higher or lower. */
+int five() {
+  int guessed = FALSE;
+  int numToGuess = rand() % 100;
+  int numUserGuessed;
+  printf("Section 5\n");
+  while (guessed == FALSE) {
+    printf("Enter a number: ");
+    scanf("%d", &numUserGuessed);
+    printf("You guessed: %i\n", numUserGuessed);
+    if (numUserGuessed == numToGuess) {
+      guessed = TRUE;
+    } else if (numUserGuessed < numToGuess) {
+      printf("Guess higher.\n");
+    } else {
+      printf("Guess lower.\n");
+    }
+  }
+  printf("The number was guessed: %i\n\n", numToGuess);
+  return 0;
+}
 
+// 6. Strings
+/* make a password guesser based on set password and an input string of a
+maximum of 20 characters */
+int six() {
+  char *setPassword = "password";
+  char input[20];
+  int guessed = FALSE;
+  printf("Section 6\n");
+  while (guessed == FALSE) {
+    printf("What's the password? Max 19 characters: ");
+    scanf("%s", input);
+    if (strncmp(setPassword, input, 20) == 0) {
+      guessed = TRUE;
+    } else {
+      printf("Incorrect password. Please try again\n");
+    }
+  }
+  printf("Password Accepted\n\n");
+  return 0;
+}
+
+// 7. For loops
+/* create an array of 8 values with exponentially increasing values using a
+ * for loop (not the math class) */
+int seven() {
+  int array[9];
+  int index;
+  array[0] = 2;
+  for (index = 1; index < 8; index++) {
+    array[index] = array[index - 1] * array[0];
+  }
+  printf("Section 7\n{ ");
+  for (index = 0; index < 8; index++) {
+    printf("%i ", array[index]);
+  }
+  printf("}\n\n");
   return 0;
 }
